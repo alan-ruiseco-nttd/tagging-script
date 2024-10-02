5110cb0abcf95a682ae27cd6d206ca51f679b9b5
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-10-02 20:57:33 +0000

    add wf action to be trigger nightly

--- main_tagging.go
+++ main_tagging.go
@@ -23,15 +23,14 @@ func main() {
 	keywordTopics := getKeyWords()
 
 	// COMMENT THIS TO AVOID UPDATING ALL REPOS WHILE TESTING
-	allRepos := getAllRepositories(ctx, client, org)
-	for _, repo := range allRepos {
-		repoName := *repo.Name
-		//
+	// allRepos := getAllRepositories(ctx, client, org)
+	// for _, repo := range allRepos {
+	// 	repoName := *repo.Name
+	//
 
-		// TEST REPOS ONLY
-		// testRepos := []string{"tf-azurerm-module_reference-key_vault", "tf-aws-module_primitive-firehose_delivery_stream"}
-		// for _, repoName := range testRepos {
-		//
+	// TEST REPOS ONLY
+	testRepos := []string{"tf-azurerm-module_reference-key_vault", "tf-aws-module_primitive-firehose_delivery_stream"}
+	for _, repoName := range testRepos {
 
 		topics := getCurrentTopics(ctx, client, org, repoName)
 
